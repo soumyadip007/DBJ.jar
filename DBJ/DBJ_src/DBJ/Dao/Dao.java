50662becbaa0a5a38531c1e6ff8ef55d2433f787
soumyadip007 <soumyadip.note@gmail.com>
2019-08-02 11:16:39 +0530

    Dao COnfig

---
+++ DBJ/DBJ_src/DBJ/Dao/Dao.java
@@ -0,0 +1,27 @@
+package DBJ.Dao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.dbj.Connection.Connect;
+
+class Dao {
+	
+	public static void main(String args[]){
+	
+		try {
+			Connection con=(Connection) Connect.dbcon();
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			
+			e.printStackTrace();
+		}
+	
+	
+	}
+		
+	}
