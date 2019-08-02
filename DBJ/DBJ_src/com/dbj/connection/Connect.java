f94e7296488dec124b5368579039541bd2ceffd3
soumyadip007 <soumyadip.note@gmail.com>
2019-08-02 11:20:23 +0530

    Function Built

--- DBJ/DBJ_src/com/dbj/connection/Connect.java
+++ DBJ/DBJ_src/com/dbj/connection/Connect.java
@@ -8,10 +8,14 @@ import com.mysql.jdbc.Connection;
 
 public class Connect {
 
-	public static  Connection  dbcon() throws ClassNotFoundException, SQLException {
+	public static  Connection  dbcon(String Driver,String Path,String U,String P) throws ClassNotFoundException, SQLException {
 		
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/resell","root","");
+		
+		Class.forName(Driver);
+		Connection con=(Connection) DriverManager.getConnection(Path,U,P);
+		
+//		Class.forName("com.mysql.jdbc.Driver");
+//		Connection con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/resell","root","");
 		
 	return con;
 }	
