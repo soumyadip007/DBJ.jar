659e808a50b867cdd76b1211934fa57210d2d025
soumyadip007 <soumyadip.note@gmail.com>
2019-08-02 12:43:06 +0530

    Config

--- DBJ/DBJ_src/com/dbj/connection/Connect.java
+++ DBJ/DBJ_src/com/dbj/connection/Connect.java
@@ -3,11 +3,16 @@ package com.dbj.connection;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
+
 import com.mysql.jdbc.Connection;
 
 
 public class Connect {
 
+	private final static Logger LOGGER =Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); 
+		
+	
 	public static  Connection  dbcon(String Driver,String Path,String U,String P) throws ClassNotFoundException, SQLException {
 		
 		
