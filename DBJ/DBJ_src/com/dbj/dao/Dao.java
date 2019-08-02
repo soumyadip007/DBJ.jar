7774b695fcb416949c3ffcbe0a621885a037c31d
soumyadip007 <soumyadip.note@gmail.com>
2019-08-02 11:22:00 +0530

    DAO Tested

--- DBJ/DBJ_src/com/dbj/dao/Dao.java
+++ DBJ/DBJ_src/com/dbj/dao/Dao.java
@@ -11,7 +11,7 @@ class Dao {
 	public static void main(String args[]){
 	
 		try {
-			Connection con=(Connection) Connect.dbcon();
+			Connection con=(Connection) Connect.dbcon("com.mysql.jdbc.Driver","jdbc:mysql://localhost:3306/resell","root","");
 			
 		} catch (ClassNotFoundException | SQLException e) {
 			
