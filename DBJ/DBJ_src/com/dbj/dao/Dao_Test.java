0aa15d74d8e2cb29d5ea009efe5e1b9afdb08b72
soumyadip007 <soumyadip.note@gmail.com>
2019-08-03 11:27:37 +0530

    TEST QUERY ADDED

--- DBJ/DBJ_src/com/dbj/dao/Dao_Test.java
+++ DBJ/DBJ_src/com/dbj/dao/Dao_Test.java
@@ -2,17 +2,25 @@ package com.dbj.dao;
 
 
 import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.dbj.connection.Connect;
+import com.dbj.query.Insert;
 
 class Dao_Test {
+	
+	 private final static Logger LOGGER =Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); 
+		
 	
 	public static void main(String args[]){
 	
 		Connection con=(Connection) Connect.dbcon("com.mysql.jdbc.Driver","jdbc:mysql://localhost:3306/resell","root","");
 	
 	
+		String[] l={"Apple", "Banana", "Orange", "Grapes"};
+		String x;
+		LOGGER.log(Level.INFO, x=Insert.ParamOpt(l));
 	}
 		
 	}
