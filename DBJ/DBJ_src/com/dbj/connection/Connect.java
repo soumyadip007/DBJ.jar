ddcd712eb8f6a019d9f2a1114b3c0aa6161566cb
soumyadip007 <soumyadip.note@gmail.com>
2019-08-03 16:55:13 +0530

    Connect Method shift

--- DBJ/DBJ_src/com/dbj/connection/Connect.java
+++ DBJ/DBJ_src/com/dbj/connection/Connect.java
@@ -15,6 +15,32 @@ public class Connect {
 
 	public  static Connection con;
 	
+
+	public static  void  dbcon(String Driver,String Path,String U,String P) {
+		
+		
+		try {
+			Class.forName(Driver);
+		} catch (ClassNotFoundException e) {
+			
+			LOGGER.log(Level.WARNING, "ClassNotFoundException");
+			e.printStackTrace();
+		}
+		try {
+			con=(Connection) DriverManager.getConnection(Path,U,P);
+		
+		} catch (SQLException e) {
+			
+			LOGGER.log(Level.WARNING, "SQLException");
+			e.printStackTrace();
+		}
+
+		LOGGER.log(Level.INFO, "Connection established \nDriver:"
+				+ Driver+"\nPath:"+Path+"\nUsername:"+U+"\nPassword:"+P);
+	
+	}	
+	
+	
 	public static  Connection  Getdbcon(String Driver,String Path,String U,String P) {
 		
 		
@@ -41,28 +67,5 @@ public class Connect {
 	
 	}	
 	
-	public static  void  dbcon(String Driver,String Path,String U,String P) {
-		
-		
-		try {
-			Class.forName(Driver);
-		} catch (ClassNotFoundException e) {
-			
-			LOGGER.log(Level.WARNING, "ClassNotFoundException");
-			e.printStackTrace();
-		}
-		try {
-			con=(Connection) DriverManager.getConnection(Path,U,P);
-		
-		} catch (SQLException e) {
-			
-			LOGGER.log(Level.WARNING, "SQLException");
-			e.printStackTrace();
-		}
-
-		LOGGER.log(Level.INFO, "Connection established \nDriver:"
-				+ Driver+"\nPath:"+Path+"\nUsername:"+U+"\nPassword:"+P);
-	
-	}	
 }
 
