94d13222a6af8e4365d6ba2195265b0c95c94dd3
soumyadip007 <soumyadip.note@gmail.com>
2019-08-03 18:05:53 +0530

    Insert method override with con

--- DBJ/DBJ_src/com/dbj/query/Insert.java
+++ DBJ/DBJ_src/com/dbj/query/Insert.java
@@ -106,6 +106,58 @@ public class Insert {
 	}
 	
 	
+	public static void Insert(Connection con,String table,String[] param,String[] value) {
+
+		if(param.length!=value.length)
+		{
+			LOGGER.log(Level.INFO,"Mismatch with parameters and values\nexit(0)");
+			return ;
+		}
+		
+		String paramResult;
+		LOGGER.log(Level.INFO, paramResult=ParamOpt(param));
+		
+		String valueResult;
+		LOGGER.log(Level.INFO, valueResult=getMark(value));
+		int n=value.length;
+		
+		
+		
+		String mainStmt="INSERT INTO "+table+"("+paramResult+") values("+valueResult+")";
+		LOGGER.log(Level.INFO, mainStmt);
+		
+		try {
+			PreparedStatement st=con.prepareStatement(mainStmt);
+			int i=0;
+			while(i<n)
+			{
+				LOGGER.log(Level.INFO, "Wrapping value: "+value[i]);
+				st.setString((i+1), value[i]);
+			i++;
+			}
+
+			int check=st.executeUpdate();
+			if(check==1)
+			{
+				LOGGER.log(Level.INFO, "Data Successfully added");
+			}
+			else
+			{
+				LOGGER.log(Level.INFO, "Database connection failed");
+			}
+			
+		} catch (SQLException e) {
+			
+			LOGGER.log(Level.WARNING, "SQLException");
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+	
+	
 	public static void main(String args[]) {
 
 	Connect.Connect("com.mysql.jdbc.Driver","jdbc:mysql://localhost:3306/springcurd","root","");
