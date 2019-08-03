c930cfe8d776f64ba66ddb4f642676de590e230a
soumyadip007 <soumyadip.note@gmail.com>
2019-08-03 19:00:43 +0530

    Driver config

--- DBJ/DBJ_src/com/dbj/dao/Dao_Test.java
+++ DBJ/DBJ_src/com/dbj/dao/Dao_Test.java
@@ -2,11 +2,14 @@ package com.dbj.dao;
 
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.dbj.connection.Connect;
 import com.dbj.query.Insert;
+import com.dbj.query.Select;
 
 class Dao_Test {
 	
@@ -18,10 +21,19 @@ class Dao_Test {
 		Connection con=(Connection) Connect.GetConnect("com.mysql.jdbc.Driver","jdbc:mysql://localhost:3306/resell","root","");
 	
 		Connect.Connect("com.mysql.jdbc.Driver","jdbc:mysql://localhost:3306/springcurd","root","");
-		String[] index={"first_name", "last_name", "email"};
-		String[] value={"Soumyadip", "Choudhury", "x@gmail.com"};
-		Insert.Save("customer",index,value);
-		
+	//	String[] index={"first_name", "last_name", "email"};
+	//	String[] value={"Soumyadip", "Choudhury", "x@gmail.com"};
+	//	Insert.Save("customer",index,value);
+		ResultSet rs=Select.GetAll("customer");
+		try {
+			while(rs.next())
+			{
+				System.out.println(rs.getString(2));
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
 	
 	}
 		
