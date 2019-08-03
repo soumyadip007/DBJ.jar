5ccab39bc5b6d0e21da63010f36379150b9b71eb
soumyadip007 <soumyadip.note@gmail.com>
2019-08-03 19:00:35 +0530

    Select stmt aded

--- DBJ/DBJ_src/com/dbj/query/Select.java
+++ DBJ/DBJ_src/com/dbj/query/Select.java
@@ -1,9 +1,59 @@
 package com.dbj.query;
 
-public class Select {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	public static void Get()
+import com.dbj.connection.Connect;
+
+public class Select {
+	
+	private final static Logger LOGGER =Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); 
+		
+	public static ResultSet rs;
+	
+	public static ResultSet GetAll(String table)
 	{
 		
+		LOGGER.log(Level.INFO,"Fetch from : "+table);
+		
+		String mainStmt="Select * from "+table;
+		
+		try {
+			PreparedStatement st=Connect.con.prepareStatement(mainStmt);
+		
+
+			rs=st.executeQuery();
+			if(rs!=null)
+			{
+				LOGGER.log(Level.INFO, "Data Successfully retrived");
+				return rs;
+			}
+			else
+			{
+				LOGGER.log(Level.INFO, "Database connection failed");
+				return rs;
+			}
+			
+		} catch (SQLException e) {
+			
+			LOGGER.log(Level.WARNING, "SQLException");
+			e.printStackTrace();
+		}		
+		
+		return rs;
 	}
+	
+	public static ResultSet GetAll(Connection con,String table)
+	{
+		
+		
+		
+		return rs;
+	}
+	
+	
 }
