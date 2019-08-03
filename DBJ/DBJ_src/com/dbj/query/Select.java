d057935a0afb717e90f32acaa09d906f99aebf1c
soumyadip007 <soumyadip.note@gmail.com>
2019-08-03 22:41:57 +0530

    Get config

--- DBJ/DBJ_src/com/dbj/query/Select.java
+++ DBJ/DBJ_src/com/dbj/query/Select.java
@@ -231,7 +231,7 @@ public class Select {
 	{
 		LOGGER.log(Level.INFO,"Fetch from : "+table);
 		
-		String mainStmt="Select * from "+table+" where "+index+"='"+value+"'";
+		String mainStmt="Select * from "+table+" where "+genAnd(index,value);
 		
 		
 		LOGGER.log(Level.INFO,"Query : "+mainStmt);
